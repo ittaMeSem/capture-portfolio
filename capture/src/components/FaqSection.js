48750f1ebdf478e6ae51bd54824d11276d304c27
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-28 15:52:56 +0300

    4.Faq section

---
+++ capture/src/components/FaqSection.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magni
+            nulla qui, praesentium adipisci nihil.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magni
+            nulla qui, praesentium adipisci nihil.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magni
+            nulla qui, praesentium adipisci nihil.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magni
+            nulla qui, praesentium adipisci nihil.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
