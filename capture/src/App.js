3472a6fdc6a275f6434eec5c722fe30015b40f06
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-28 16:20:42 +0300

    5.About section styled components

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 //Import pages
 import AboutUs from "./pages/AboutUs";
+//Global style
+import GLobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <GLobalStyle />
     </div>
   );
 }
